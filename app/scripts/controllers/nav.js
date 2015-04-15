4e26ee38d2ed0c86fd4f6837f433e4ccacad5cec
suryatejahanumandla <suryatejahanumandla@gmail.com>
2015-04-15 11:27:30 +0530

    applibrary,dropdown updated and modal removed

--- app/scripts/controllers/nav.js
+++ app/scripts/controllers/nav.js
@@ -13,17 +13,17 @@
 			controller : function($scope, $modal, $log) {
 
 				$scope.items = ['Chart', 'App2', 'APP3'];
-
-				$scope.open = function(size) {
-					var url;
-					if (size == "lg") {
-						url = '/views/notifications.html'
-
-					} else {
-						url = '/views/applibrary.html'
+				$scope.libraryVisible = false;
+				$scope.toggle = function() {
+					if ($scope.libraryVisible) {
+						$scope.libraryVisible = !$scope.libraryVisible;
 					}
+
+				};
+				$scope.open = function(size) {
+
 					var modalInstance = $modal.open({
-						templateUrl : url,
+						templateUrl : '/views/notifications.html',
 						controller : 'ModalInstanceCtrl',
 						backdrop : 'static',
 						size : size,
@@ -33,16 +33,200 @@
 							}
 						}
 					});
-
-					modalInstance.result.then(function(selectedItem) {
-						$scope.selected = selectedItem;
-					}, function() {
-						$log.info('Modal dismissed at: ' + new Date());
-					});
 				};
 
 			},
 			controllerAs : 'navCtrl'
+		};
+	});
+	app.directive('', function() {
+	});
+	app.directive('appLibrary', function() {
+		return {
+			restrict : 'E',
+			templateUrl : '/views/applibrary.html',
+			controller : function($scope) {
+
+				$scope.isAssetOpen = false;
+
+				$scope.isNetworkDesignOpen = false;
+
+				$scope.isServicesOpen = false;
+
+				$scope.isTroubleshootingOpen = false;
+
+				$scope.ShowAppList = function($event) {
+
+					var name = $event.currentTarget.innerText || $event.currentTarget.textContent;
+
+					switch(name) {
+					case "Asset Management" :
+						$scope.isAssetOpen = true;
+						$scope.isNetworkDesignOpen = false;
+						$scope.isServicesOpen = false;
+						$scope.isTroubleshootingOpen = false;
+						break;
+					case "Network Design":
+						$scope.isAssetOpen = false;
+						$scope.isNetworkDesignOpen = true;
+						$scope.isServicesOpen = false;
+						$scope.isTroubleshootingOpen = false;
+						break;
+					case "Services":
+						$scope.isAssetOpen = false;
+						$scope.isNetworkDesignOpen = false;
+						$scope.isServicesOpen = true;
+						$scope.isTroubleshootingOpen = false;
+						break;
+					case "Troubleshooting":
+						$scope.isAssetOpen = false;
+						$scope.isNetworkDesignOpen = false;
+						$scope.isServicesOpen = false;
+						$scope.isTroubleshootingOpen = true;
+						break;
+					};
+
+				};
+			},
+			controllerAs : 'appLibraryCtrl'
+		};
+
+	});
+	app.directive('assetManagement', function() {
+		return {
+			restrict : 'E',
+			templateUrl : 'views/appLibrary/assetManagement.html',
+			controller : function($scope) {
+				$scope.assetList = [{
+					name : 'ALARM',
+					status : 'DOWNLOADED',
+					updateAvailable : false
+				}, {
+					name : 'PM APP',
+					status : 'DOWNLOADED',
+					updateAvailable : true
+				}, {
+					name : 'PM APP',
+					status : 'DOWNLOADED',
+					updateAvailable : false
+				}, {
+					name : 'ALARM',
+					status : 'DOWNLOADED',
+					updateAvailable : false
+				}, {
+					name : 'PM APP',
+					status : 'DOWNLOADED',
+					updateAvailable : true
+				}, {
+					name : 'PM APP',
+					status : 'DOWNLOADED',
+					updateAvailable : false
+				}];
+			},
+			controllerAs : 'assetCtrl'
+		};
+	});
+
+	app.directive('networkDesign', function() {
+		return {
+			restrict : 'E',
+			templateUrl : 'views/appLibrary/networkDesign.html',
+			controller : function($scope) {
+				$scope.networkDesignList = [{
+					name : 'ALARM',
+					status : 'DOWNLOADED',
+					updateAvailable : false
+				}, {
+					name : 'PM APP',
+					status : 'DOWNLOADED',
+					updateAvailable : true
+				}, {
+					name : 'PM APP',
+					status : 'DOWNLOADED',
+					updateAvailable : false
+				}, {
+					name : 'ALARM',
+					status : 'DOWNLOADED',
+					updateAvailable : false
+				}, {
+					name : 'PM APP',
+					status : 'DOWNLOADED',
+					updateAvailable : true
+				}, {
+					name : 'PM APP',
+					status : 'DOWNLOADED',
+					updateAvailable : false
+				}];
+			},
+			controllerAs : 'networkDesignCtrl'
+		};
+	});
+	app.directive('services', function() {
+		return {
+			restrict : 'E',
+			templateUrl : 'views/appLibrary/services.html',
+			controller : function($scope) {
+				$scope.servicesList = [{
+					name : 'ALARM',
+					status : 'DOWNLOADED',
+					updateAvailable : false
+				}, {
+					name : 'PM APP',
+					status : 'DOWNLOADED',
+					updateAvailable : true
+				}, {
+					name : 'PM APP',
+					status : 'DOWNLOADED',
+					updateAvailable : false
+				}, {
+					name : 'ALARM',
+					status : 'DOWNLOADED',
+					updateAvailable : false
+				}, {
+					name : 'PM APP',
+					status : 'DOWNLOADED',
+					updateAvailable : true
+				}, {
+					name : 'PM APP',
+					status : 'DOWNLOADED',
+					updateAvailable : false
+				}];
+			},
+			controllerAs : 'servicesCtrl'
+		};
+	});
+	app.directive('troubleshooting', function() {
+		return {
+			restrict : 'E',
+			templateUrl : 'views/appLibrary/troubleshooting.html',
+			controller : function($scope) {
+				$scope.troubleshootingList = [{
+					name : 'DEMO',
+					status : 'DOWNLOADED',
+					updateAvailable : true
+				}, {
+					name : 'PM APP',
+					status : 'DOWNLOADED',
+					updateAvailable : false
+				}, {
+					name : 'PM APP',
+					status : 'DOWNLOADED',
+					updateAvailable : false
+				}, {
+					name : 'ALARM',
+					status : 'DOWNLOADED',
+					updateAvailable : false
+				}, {
+					name : 'PM APP',
+					status : 'DOWNLOADED',
+					updateAvailable : false
+				}, {
+					name : 'PM APP',
+					status : 'DOWNLOADED',
+					updateAvailable : true
+				}];
+			},
+			controllerAs : 'troubleshootingCtrl'
 		};
 	});
 	// Please note that $modalInstance represents a modal window (instance) dependency.
@@ -55,47 +239,6 @@
 
 		$scope.ok = function() {
 			$modalInstance.close($scope.selected.item);
-		};
-
-		$scope.isAssetOpen = false;
-
-		$scope.isNetworkDesignOpen = false;
-
-		$scope.isServicesOpen = false;
-
-		$scope.isTroubleshootingOpen = false;
-
-		$scope.appList = function($event) {
-
-			var name = $event.currentTarget.innerText || $event.currentTarget.textContent;
-
-			switch(name) {
-			case "Asset Management" :
-				$scope.isAssetOpen = true;
-				$scope.isNetworkDesignOpen = false;
-				$scope.isServicesOpen = false;
-				$scope.isTroubleshootingOpen = false;
-				break;
-			case "Network Design":
-				$scope.isAssetOpen = false;
-				$scope.isNetworkDesignOpen = true;
-				$scope.isServicesOpen = false;
-				$scope.isTroubleshootingOpen = false;
-				break;
-			case "Services":
-			    $scope.isAssetOpen = false;
-				$scope.isNetworkDesignOpen = false;
-				$scope.isServicesOpen = true;
-				$scope.isTroubleshootingOpen = false;
-				break;
-			case "Troubleshooting":
-				$scope.isAssetOpen = false;
-				$scope.isNetworkDesignOpen = false;
-				$scope.isServicesOpen = false;
-				$scope.isTroubleshootingOpen = true;
-				break;
-			};
-
 		};
 
 		$scope.cancel = function() {
